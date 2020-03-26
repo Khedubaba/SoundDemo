764bb9cb2c8dbb3130a4c2307f5c449177c61646
khedubaba <adityakhedekar49@gmail.com>
2020-03-26 17:19:37 +0530

    c3

--- app/src/main/java/com/codellion/sounddemo/MainActivity.java
+++ app/src/main/java/com/codellion/sounddemo/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
                 prg = progress;
                 prg = dragProgress;
                 //mediaPlayer.seekTo(progress);
+//
             }
 
             @Override
